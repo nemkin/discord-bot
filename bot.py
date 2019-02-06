f4f55bac5adcf83aa74e6163a930846b67071dba
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2019-02-06 12:54:24 +0000

    Requirements, mmr out.

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    print("(" + message.channel.name + ") " +\
-          message.author.name  + ": " +\
+    print('(' + message.channel.name + ') ' +\
+          message.author.name  + ': ' +\
           message.content)
 
     #!hello
@@ -21,7 +21,7 @@ async def on_message(message):
     #!vicc
     if message.content.startswith('!vicc'):
         result = requests.get(url='http://api.icndb.com/jokes/random')
-        msg = result.json()['value']['joke']
+        msg = result.json()['value']['joke'].replace('&quot;', '"')
         await client.send_message(message.channel, msg)
 
     #!cica
@@ -30,15 +30,10 @@ async def on_message(message):
         msg = result.json()[0]['url']
         await client.send_message(message.channel, msg)
 
-    #!mmr
-    if message.content.startswith('!mmr'):
-        msg = "Dota2 MMR:\n"
-        people = {"Ricsi": "105340260", "Misi": "167548908", "Imi": "49933861"}
-        for person, dota_id in people.items():
-            result = requests.get('https://api.opendota.com/api/players/' + dota_id) 
-            mmr = result.json()['mmr_estimate']['estimate']
-            player = result.json()['profile']['personaname']
-            msg += person + ": " + str(mmr) + " (" + player + ")\n"
+    #!roka
+    if message.content.startswith('!roka'):
+        result = requests.get('https://randomfox.ca/floof')
+        msg = result.json()['image']
         await client.send_message(message.channel, msg)
 
     if message.channel.name != 'bottest':
