5e6b1d6d946a7351ccbe5ea40c19ecea74686200
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-01-30 23:18:48 +0100

    Add

---
+++ hank.js
@@ -0,0 +1,72 @@
+import { Client, Intents, MessageEmbed } from "discord.js";
+
+import cron from "node-cron";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+class Media {
+  static async cat() {
+    const url = "https://api.thecatapi.com/v1/images/search?mime_types=gif";
+    const response = await fetch(url, {
+      headers: { "x-api-key": process.env.CAT_API_KEY },
+    });
+    const json = await response.json();
+    return json[0].url;
+  }
+
+  static fu() {
+    return "https://c.tenor.com/vKqX9v3pAEIAAAAC/flipping-off-flip-off.gif";
+  }
+
+  static hearts() {
+    return "https://cdn.discordapp.com/emojis/809508000100122685.gif";
+  }
+
+  static cryCat() {
+    return "https://cdn.discordapp.com/emojis/922190379791581274.webp";
+  }
+
+  static eatCat() {
+    return "https://c.tenor.com/IMYZL-7t2ucAAAAC/cat-butt.gif";
+  }
+}
+
+client.once("ready", async () => {
+  const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+  const user = await client.users.fetch(process.env.USER_ID);
+
+  const message = new MessageEmbed()
+    .setDescription("Hast du ja gegessen? :eyes:\n\n" + user.toString())
+    .setAuthor({
+      name: "Hallo Tinny!",
+      iconURL: Media.hearts(),
+    })
+    .setFooter({
+      text: "Bitte?",
+      iconURL: Media.cryCat(),
+    })
+    .setColor("#BD93F9")
+    .setImage(Media.eatCat());
+    
+  cron.schedule("0 11,13,17,19 * * *", () => {
+    channel.send({ embeds: [message] });
+  });
+});
+
+client.on("message", async (message) => {
+  const msg = message.content;
+  if (msg.startsWith("!hello")) {
+    const cat = await Media.cat();
+    message.reply(`Is it me you're looking for ${message.author}?`);
+    message.channel.send(cat);
+  } else if (msg.startsWith("!fu")) {
+    const fu = Media.fu();
+    message.reply(fu);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
--- hank.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "RUN" >> /home/vikto/output.txt
-/usr/bin/node /home/vikto/discord-bot/hank_message.js 2>&1 >> /home/vikto/output.txt
-echo "DONE" >> /home/vikto/output.txt
-
